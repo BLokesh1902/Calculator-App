7a3a3708dac5582d2d8d44605e0873421c82e967
BLokesh1902 <balusulokesh1234@gmail.com>
2024-02-14 20:03:58 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import styles from "./App.module.css";
+import { useState } from "react";
+
+function App() {
+     
+  const[calval,setCalval]=useState("")
+
+  const onButtonClick=(buttonText) => {
+    if(buttonText==='C'){
+      setCalval("")
+    }
+    else if(buttonText==='='){
+      const result=eval(calval)
+      setCalval(result)
+    }
+    else{
+      const newDisplayValue=calval+buttonText;
+      setCalval(newDisplayValue)
+    }
+  }
+
+
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calval}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+  );
+}
+
+export default App;
